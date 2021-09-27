18e4d8c0dc25796fe14b2837f9b34bf64a7e6f81
dbsdnwjd96 <sally@geekstudio.co.kr>
2021-09-27 20:43:26 +0900

    chapter1

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+// import './components/nav';
+// import './components/footer';
+// import './components/home';
+// import './components/exchange';
+// import './components/map';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Nav() {
+    return (
+      <nav>
+      <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      <li></li>
+      </ul>
+      </nav>
+    );
+  }
