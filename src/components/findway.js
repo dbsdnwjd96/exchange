da6bf3210711b83baa83bf41da98b29e8c61f2e5
dbsdnwjd96 <sally@geekstudio.co.kr>
2021-09-30 19:01:40 +0900

    Findway/ connect map api

--- src/components/findway.js
+++ src/components/findway.js
@@ -1,9 +1,28 @@
-import React from 'react';
+// eslint-disable-next-line
+import React from "react";
+import CreateMap from './map';
 
 function Findway() {
     return (
+        <>
         <div>오시는길</div>
+        <CreateMap />
+        </>
     );
 }
 
+function Address() {
+    return (
+        <div className="address">
+            <ul>
+                <li></li>
+                <li></li>
+                <li></li>
+            </ul>
+        </div>
+    );
+}
+
+
 export default Findway;
+
